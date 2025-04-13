2f7b339395070eaece4a5b917ed9f418fb879383
Vlad Prokopiy <vladprokopiy@gmail.com>
2025-04-14 02:14:16 +0300

    fix

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function makeArray(firstArray, secondArray, maxLength) {
   const newArray = firstArray.concat(secondArray);
   if (newArray.length >= maxLength) {
     return newArray.slice(0, maxLength);
-  } else return;
+  } else return [];
 }
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
